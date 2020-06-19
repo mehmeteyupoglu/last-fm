dc9ccbed9a03162033176a807cbf3c341bc4c67d
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-06-19 19:19:44 +0300

    Update data

--- last-fm/src/data.js
+++ last-fm/src/data.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
-export const data = axios.get('http://ws.audioscrobbler.com/2.0/?method=geo.getTopTracks&country=spain&api_key=97cee60fe2193b383cd8377301901a80&format=json')
+export const data1 = () => {axios.get('http://ws.audioscrobbler.com/2.0/?method=geo.getTopTracks&country=spain&limit=10&api_key=97cee60fe2193b383cd8377301901a80&format=json')
   .then(function (response) {
     // handle success
-    console.log(response);
+    console.log(`Data 1: `, response);
   })
   .catch(function (error) {
     // handle error
@@ -11,6 +11,17 @@ export const data = axios.get('http://ws.audioscrobbler.com/2.0/?method=geo.getT
   })
   .then(function () {
     // always executed
-  });
+  });}
 
-  
+export const data2 = axios.get('http://ws.audioscrobbler.com/2.0/?method=geo.getTopArtists&country=spain&limit=10&api_key=97cee60fe2193b383cd8377301901a80&format=json')
+.then(function (response) {
+  // handle success
+  console.log(`Data 2: `, response);
+})
+.catch(function (error) {
+  // handle error
+  console.log(error);
+})
+.then(function () {
+  // always executed
+});
