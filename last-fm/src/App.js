adf9eeeedbd4e086ebdb68a30129bd3bfc416d8f
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-06-19 17:48:05 +0300

    Miss the initial commit

---
+++ last-fm/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header, MainBody } from "./Components"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainBody />
+    </div>
+  );
+}
+
+export default App;
