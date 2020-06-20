e51ac308187438401a45b259dfb3d79cdb372837
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-06-20 15:03:49 +0300

    Refactor App.js

--- last-fm/src/App.js
+++ last-fm/src/App.js
@@ -5,11 +5,11 @@ import { Header, TopArtists, TopTracks } from "./Components"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-          <TopArtists />
-          <TopTracks />
-    </div>
+      <div className="App">
+        <Header />
+        <TopArtists />
+        <TopTracks />
+      </div>
   );
 }
 
