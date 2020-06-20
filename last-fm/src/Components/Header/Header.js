67877188a984a05ed03adbbfc16f2637aa03fe07
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-06-20 12:35:47 +0300

    Add last fm logo in the header

--- last-fm/src/Components/Header/Header.js
+++ last-fm/src/Components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { StyledHeader } from "./styles"
 
 const Header = () => {
     
     return (
         <div>
-            <h1>Last FM</h1>
+            <StyledHeader>
+                <img 
+                src="https://fontmeme.com/images/Last.fm-Logo.jpg" 
+                alt="Last Fm logo"
+                className="logo"   
+                />
+            </StyledHeader>
         </div>
     );
 }
---
+++ last-fm/src/Components/Header/styles.js
@@ -0,0 +1,10 @@
+import Styled from "styled-components"
+
+export const StyledHeader = Styled.div`
+
+img {
+    width: 150px; 
+    height: 150px; 
+}
+
+`
